6aea3e7920e17d85b7b66a9007553948995d5542
ahmedswilam <ahmedswelam141@gmail.com>
2023-02-05 18:06:48 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,65 @@
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  entry: {
+    app: "./src/index.js",
+  },
+
+  output: {
+    path: path.join(__dirname, "/dist"),
+    publicPath: "",
+    filename: "main.js",
+  },
+
+  mode: "development",
+  
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [{
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: '../'
+            }
+          },
+          'css-loader',
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/style.css"
+    }),
+  ],
+};
