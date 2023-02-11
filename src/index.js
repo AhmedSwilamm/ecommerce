498b6f9ba06a4418707d181f7fd026dc9c757074
ahmedswilam <ahmedswelam141@gmail.com>
2023-02-11 14:56:55 +0200

    carousel feature

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
 import './css/style.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+import '@laylazi/bootstrap-rtl/dist/css/bootstrap-rtl.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import 'jquery/dist/jquery.min';
 import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import '@fortawesome/fontawesome-free/js/all.min';
+
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
   entry: {
     app: "./src/index.js",
@@ -8,8 +9,9 @@ module.exports = {
 
   output: {
     path: path.join(__dirname, "/dist"),
-    publicPath: "",
+    // publicPath: "",
     filename: "main.js",
+    assetModuleFilename: 'images/[name].[ext]',
   },
 
   mode: "development",
@@ -32,23 +34,47 @@ module.exports = {
         use: [{
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: '../'
+              publicPath: '../',
             }
           },
           'css-loader',
         ]
       },
       {
-        test: /\.(png|jpe?g|gif)$/,
+        test: /\.(png|jpe?g|gif)/,
+        type: 'asset/resource'
+      },
+      // {
+      //   test: /\.(png|jpe?g|gif)$/,
+      //   use: [
+      //     {
+      //       loader: "file-loader",
+      //       options: {
+      //         name: "[name].[ext]",
+      //         outputPath: "./images",
+      //       },
+      //     },
+      //   ],
+      // },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[name].[ext]',
-              outputPath: 'images',
+              name: "[name].[ext]",
+              outputPath: "fonts",
+              esModule: false,
             },
           },
         ],
+      },
+      {
+        test: require.resolve("jquery"),
+        loader: "expose-loader",
+        options: {
+          exposes: ["$", "jQuery"],
+        },
       },
     ],
   },
