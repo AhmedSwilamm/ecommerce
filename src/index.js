e2dfd630fb0891b8dfdde7de1755fe36c0ba6728
ahmedswilam <ahmedswelam141@gmail.com>
2023-02-28 15:04:25 +0200

    carousel feature

--- src/index.js
+++ src/index.js
@@ -20,4 +20,73 @@ $(document).ready(function() {
         $(this).parents('.product-option').addClass('active');
     });
 
+    $('[data-product-quantity]').change(function() {
+        var newQuantity= $(this).val();
+        var parent = $(this).parents('[data-product-info]');
+        var pricePerUnit = parent.attr('data-product-price');
+        var totalPriceForProduct = newQuantity * pricePerUnit;
+        parent.find('.total-price-for-product').text(totalPriceForProduct + '$');
+        calculateTotalPrice();
+    });
+
+    $('[data-remove-from-cart]').click(function() {
+        $(this).parents('[data-product-info]').remove();
+        calculateTotalPrice();
+    });
+    function calculateTotalPrice() {
+        var totalPriceForAllProduct = 0;
+        $('[data-product-info]').each(function() {
+            var pricePerUnit = $(this).attr('data-product-price');
+            var quantity = $(this).find('[data-product-quantity]').val();
+            var totalPriceForProduct = pricePerUnit * quantity;
+            totalPriceForAllProduct = totalPriceForAllProduct + totalPriceForProduct;
+        });
+        $('#total-price-for-all-products').text(totalPriceForAllProduct + '$')
+    }
+
+    var citiesByCountry = {
+        sa: ['الرياض','جدة'],  
+        eg: ['القاهرة','الإسكندرية'],   
+        jo: ['عمان','الزرقاء'],     
+        sy: ['دمشق','حلب','حماه'] 
+    };
+
+    $('#form-checkout select[name=\"country\"]').change(function() {
+        var country = $(this).val();
+        var cities = citiesByCountry[country];
+        $('#form-checkout select[name=\"city\"]').empty();
+        $('#form-checkout select[name=\"city\"]').append(
+            '<option disabled selected value=\"\">اختر المدينة</option>'
+        );    
+        cities.forEach(function(city) {
+            var newOption = $('<option></option>');
+            newOption.text(city);
+            newOption.val(city);
+            $('#form-checkout select[name=\"city\"]').append(newOption);
+        });
+    });
+    
+    $('#form-checkout input[name=\"payment_method\"]').change(function() {
+        var paymentMethod = $(this).val();
+        if (paymentMethod === 'on_delivery') {
+            $('#credit-card-info input').prop('disabled', true);
+        } else {
+            $('#credit-card-info input').prop('disabled', false);
+        }
+
+        $('#credit-card-info').toggle();
+        });
+
+        // $( \"#price-range\" ).slider({
+        //     range: true,
+        //     min: 50,
+        //     max: 1000,
+        //     step: 50,
+        //     values: [ 250, 800 ],
+        //     slide: function( event, ui ) {
+        //         $('#price-min').text(ui.values[0]);
+        //         $('#price-max').text(ui.values[1]);
+        //     }
+        // });
+
 });
